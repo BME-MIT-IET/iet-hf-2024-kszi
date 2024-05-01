44bfb0fe774504098cd5ead064bc0ad8c3920f8f
bodand <bodand@proton.me>
2024-05-01 20:08:39 +0200

    fix(ci): fix build errors
    - extract jq command into script, error probably was with random shell quoting
    - fix jdk package name

---
+++ ci-utils/list-pkg.sh
@@ -0,0 +1,12 @@
+﻿#!/usr/bin/env sh
+
+type=$1
+
+dotnet list package "--$type" --format json --output-version 1 \
+  | jq '(.projects | map({
+          proj: (.path | sub(".+?(?<p>[^/\\\\]+).csproj$"; "\n\n### \(.p)\n")),
+          deps: (.frameworks[]? | "#### \(.framework):\n" + (reduce
+            (.topLevelPackages | map("\(.id): \(.requestedVersion) -> \(.latestVersion)"))[] as $dep
+            (""; "\(.)\n - \($dep)")))
+        }))' \
+    > "$type.json"
